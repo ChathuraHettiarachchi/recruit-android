dc85e4b54541f6900ccf8f399c18c666915d7b51
Chathura Hettiarachchi <chathura93@yahoo.com>
2023-11-10 08:03:06 +1300

    update folder contents based on old files

--- app/src/main/java/nz/co/test/transactions/presentation/transactions/TransactionListActivity.kt
+++ app/src/main/java/nz/co/test/transactions/presentation/transactions/TransactionListActivity.kt
@@ -1,13 +1,13 @@
-package nz.co.test.transactions.activities
+package nz.co.test.transactions.presentation.transactions
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import nz.co.test.transactions.R
 
-class MainActivity : AppCompatActivity() {
+class TransactionListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_transaction_list)
     }
 }
