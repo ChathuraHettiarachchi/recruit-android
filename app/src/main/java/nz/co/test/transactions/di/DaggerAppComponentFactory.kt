b7eb1ced4aaa79c0264ac68a7a6778e864288b61
Chathura Hettiarachchi <chathura93@yahoo.com>
2023-11-10 08:45:46 +1300

    di got to work with vm

--- app/src/main/java/nz/co/test/transactions/di/DaggerAppComponentFactory.kt
+++ app/src/main/java/nz/co/test/transactions/di/DaggerAppComponentFactory.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.AppComponentFactory
 import androidx.core.app.ComponentActivity
+import nz.co.test.transactions.di.component.DaggerAppComponent
+import nz.co.test.transactions.presentation.transactions.TransactionListActivity
 import javax.inject.Inject
 import javax.inject.Provider
 
@@ -18,4 +20,17 @@ class DaggerAppComponentFactory : AppComponentFactory() {
 
     @Inject
     lateinit var map: Map<Class<out Activity>, @JvmSuppressWildcards Provider<Activity>>
+
+    override fun instantiateActivityCompat(
+        cl: ClassLoader,
+        className: String,
+        intent: Intent?
+    ): Activity {
+        val activityClass = cl.loadClass(className)
+
+        return if (activityClass == TransactionListActivity::class.java) {
+            val activity = map.getValue(TransactionListActivity::class.java)
+            activity.get()
+        } else super.instantiateActivityCompat(cl, className, intent)
+    }
 }
--- app/src/main/java/nz/co/test/transactions/di/component/AppComponent.kt
+++ app/src/main/java/nz/co/test/transactions/di/component/AppComponent.kt
@@ -4,15 +4,19 @@ import dagger.Component
 import nz.co.test.transactions.di.DaggerAppComponentFactory
 import nz.co.test.transactions.di.modules.ActivitiesModule
 import nz.co.test.transactions.di.modules.NetworkModule
+import nz.co.test.transactions.di.modules.ViewModelModule
+import nz.co.test.transactions.presentation.transactions.TransactionListActivity
 import javax.inject.Singleton
 
 @Singleton
 @Component(
     modules = [
         NetworkModule::class,
-        ActivitiesModule::class
+        ActivitiesModule::class,
+        ViewModelModule::class,
     ]
 )
 interface AppComponent {
     fun inject(appComponent: DaggerAppComponentFactory)
+    fun inject(transactionListActivity: TransactionListActivity)
 }
